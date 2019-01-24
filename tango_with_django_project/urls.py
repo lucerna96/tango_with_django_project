7935b038c15c27b07cefaab0d93cac015e4a615a
Luca Palkovits <2147974P@student.gla.ac.uk>
2019-01-24 21:08:02 +0100

    Chapter 3 Exercises

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     # above maps any URLs starting
     # with rango/ to be handled by
     # the rango application
+    url(r'^about/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
 ]
