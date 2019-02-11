70e1ca74829649119cc3ca872132c54fa742eb0e
Luca Palkovits <2147974P@student.gla.ac.uk>
2019-02-11 23:45:52 +0100

    Models, Templates and Views (Chapter 6)

--- populate_rango.py
+++ populate_rango.py
@@ -14,25 +14,33 @@ def populate():
 
     python_pages = [
         {"title" : "Official Python Tutorial",
-        "url" : "http://docs.python.org/2/tutorial/"},
+        "url" : "http://docs.python.org/2/tutorial/",
+        "views" : 20},
         {"title" : "How to Think like a Computer Scientist",
-        "url" : "http://www.greenteapress.com/thinkpython/"},
+        "url" : "http://www.greenteapress.com/thinkpython/",
+        "views" : 8},
         {"title" : "Learn Python in 10 Minutes",
-        "url" : "http://korokithakis.net/tutorials/python/"} ]
+        "url" : "http://korokithakis.net/tutorials/python/",
+        "views" : 100} ]
 
     django_pages = [
         {"title" : "Official Django Tutorial",
-        "url" : "http://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+        "url" : "http://docs.djangoproject.com/en/1.9/intro/tutorial01/",
+        "views" : 4},
         {"title" : "Django Rocks",
-        "url" : "http://www.djangorocks.com/"},
+        "url" : "http://www.djangorocks.com/",
+        "views" : 40},
         {"title" : "How to Tango with Django",
-        "url" : "http://www.tangowithdjango.com/"} ]
+        "url" : "http://www.tangowithdjango.com/",
+        "views" : 20} ]
 
     other_pages = [
         {"title" : "Bottle",
-        "url" : "http://bootlepy.org/docs/dev/"},
+        "url" : "http://bootlepy.org/docs/dev/",
+        "views" : 12},
         {"title" : "Flask",
-        "url" : "http://flask.pocoo.org"} ]
+        "url" : "http://flask.pocoo.org",
+        "views" : 20} ]
 
     cats = {"Python": {"pages" : python_pages, "views" : 128, "likes" : 64},
             "Django": {"pages" : django_pages, "views" : 64, "likes" : 32},
@@ -47,15 +55,16 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
 
     # Print out the categories we have added
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print("- {0} - {1}".format(str(c), str(p)))
 
-def add_page(cat, title, url, views=0):
-    p = Page.objects.get_or_create(category=cat, title=title)[0]
+# def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
+    p = Page.objects.get_or_create(category=cat, title=title, views=views)[0]
     p.url=url
     p.views=views
     p.save()
--- rango/models.py
+++ rango/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -16,6 +22,10 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Page, self).save(*args, **kwargs)
 
     def __str__(self): # For Python 2, use __unicode__ too
         return self.title
